8a5203b13e1eaff4932c2f68e88f413726a14270
Dusan Petrovic <dusanp93@live.com>
2021-07-08 17:11:45 +0200

    add bootstrap/font-awesome, create product list component

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: ` <div>
+    <h1>{{ title }}</h1>
+    <pm-products></pm-products>
+  </div>`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  title: string = 'Angular: Getting Started';
 }
